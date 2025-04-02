44429f58f85a60b5b299e7dcd3999569aeb7c5b0
MhugninnES <emilien.schmitter1@gmail.com>
2025-04-02 13:20:28 +0200

    feat: starting security

--- php/register.php
+++ php/register.php
@@ -2,12 +2,18 @@
 
 include 'config.php';
 
-$lastname = $_POST['lastname'];
-$firstname = $_POST['firstname'];
-$email = $_POST['email'];
-$password = $_POST['password'];
+$lastname = trim($_POST['lastname']);
+$firstname = trim($_POST['firstname']);
+$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    die("Email non valide");
+}
 
 $sql = "INSERT INTO customers(lastname, firstname, email, password) VALUES('$lastname', '$firstname', '$email', '$password')";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
+
+header("Location: /elegance-paris/public/login.php");
+exit;
--- public/register.php
+++ public/register.php
@@ -2,13 +2,13 @@
 
 <form action="../php/register.php" method="post" class="flex flex-col items-center gap-2 mt-6">
     <label for="lastname">Nom</label>
-    <input type="text" name="lastname">
+    <input type="text" name="lastname" required>
     <label for="firstname">Prenom</label>
-    <input type="text" name="firstname">
+    <input type="text" name="firstname" required>
     <label for="email">Email</label>
-    <input type="email" name="email">
+    <input type="email" name="email" required>
     <label for="password">Mot de passe</label>
-    <input type="password" name="password">
+    <input type="password" name="password" required>
     <input type="submit" value="Inscription" class="bg-red-300 cursor-pointer m-2 p-1">
 </form>
 <a href="login.php" class="text-xl flex justify-center mb-8">Se connecter</a>
