a6aa3cfb6bd149a12d203f495ae373a029bbf5e1
Theo Durand <theodurand88650@gmail.com>
2025-04-04 13:29:43 +0200

    Test

--- public/categorie.php
+++ public/categorie.php
@@ -3,4 +3,7 @@
   <a href="collection.php?type=chapeau">Chapeau</a>
   <a href="collection.php?type=canne">Canne</a>
   <a href="collection.php?type=gant">Gants</a>
+</div>    
+<div>
+  qsf
 </div>
