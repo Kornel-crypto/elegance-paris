6bd92b4a7eac01bffe67fd388cc255f2e1c4fb01
MhugninnES <emilien.schmitter1@gmail.com>
2025-04-04 11:36:58 +0200

    feat: adding command to send for admin

--- public/admin_page.php
+++ public/admin_page.php
@@ -1,7 +1,21 @@
-<?php include 'header.php'; 
+<?php include '../php/config.php';
+include 'header.php'; 
 
-    $sql = 'SELECT * FROM orders '
-?>
+    $sql = 'SELECT * FROM orders ORDER BY customer_email';
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($orders as $order): ?>
+
+    <div class="carte-produit">
+        <h2><?= $order['customer_email'] ?></h2>
+        <p><?= $order['product_name'] ?></p>
+        <p><?= $order['quantity'] ?></p>
+        <p><?= $order['total_price'] ?> €</p>
+    </div>
+
+<?php endforeach; ?>
 
 
 
