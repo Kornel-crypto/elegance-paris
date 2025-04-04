1ceac4700ce636457a945bcd1c5a6d14452a2434
Romain <didierjeanromain@gmail.com>
2025-04-04 11:56:09 +0200

    First front end commit. Including index, header and footer without responsive.

--- public/footer.php
+++ public/footer.php
@@ -1,5 +1,27 @@
-    <footer class="bg-gray-100 text-center text-sm text-gray-500 py-6">
-      © 2025 Élégance Parisienne - Theo, Romain, Emilien, Kornel
+</body>
+<footer>
+        <div class="h-[290px] bg-[#F1F1F1] border-t-2 border-[#0605AB] flex justify-between items-center pl-[100px] pr-[100px]">
+            <img src="images/logo.svg" alt="Logo" class="w-[170px]">
+            <div class="font-Helvetica text-[19px] font-light flex flex-col h-[150px]">
+                <h4 class="font-Helvetica text-[30px] text-left font-bold italic">Plan du site</h4>
+                <a href="">Accueil</a>
+                <a href="">Nos produits</a>
+            </div>
+            <div class="font-Helvetica text-[19px] font-light h-[150px]">
+                <h4 class="font-Helvetica text-[30px] text-left font-bold italic">Informations de contact</h4>
+                <p>eleganceparis@ipsum.fr</p>
+                <p>5 rue du Lorem, 65256 Ipsum</p>
+                <p>03 06 06 06 06</p>
+            </div>
+            <div class="font-Helvetica text-[19px] font-light h-[150px]">
+                <h4 class="font-Helvetica text-[30px] text-left font-bold italic">Politiques</h4>
+                <p>Politique de livraison</p>
+                <p>Politique de remboursement</p>
+                <p>Conditions d'utilisation</p>
+            </div>
+        </div>
+        <div class="border-t-2 border-[#D9D9D9] h-[80px] bg-[#F1F1F1] flex justify-center items-center">
+            <p class="font-Helvetica text-[18px]">Copyright 2025 - Les Gays</p>
+        </div>
     </footer>
-  </body>
 </html>
--- public/header.php
+++ public/header.php
@@ -11,37 +11,113 @@
       href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Inter:wght@400;600&display=swap"
       rel="stylesheet"
     />
+    <link href="style.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
     <style>
-      body {
-        font-family: "Inter", sans-serif;
-      }
-      h1,
-      h2,
-      h3 {
-        font-family: "Playfair Display", serif;
-      }
+      .cormorant-<uniquifier> {
+    font-family: "Cormorant", serif;
+    font-optical-sizing: auto;
+    font-weight: <weight>;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Helvetica';
+    src: url('Helvetica.woff2') format('woff2'),
+        url('Helvetica.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'Helvetica';
+    src: url('Helvetica-Oblique.woff2') format('woff2'),
+        url('Helvetica-Oblique.woff') format('woff');
+    font-weight: normal;
+    font-style: italic;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'Helvetica';
+    src: url('Helvetica-BoldOblique.woff2') format('woff2'),
+        url('Helvetica-BoldOblique.woff') format('woff');
+    font-weight: bold;
+    font-style: italic;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'Helvetica';
+    src: url('HelveticaRounded-Bold.woff2') format('woff2'),
+        url('HelveticaRounded-Bold.woff') format('woff');
+    font-weight: bold;
+    font-style: normal;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'Helvetica';
+    src: url('Helvetica-Bold.woff2') format('woff2'),
+        url('Helvetica-Bold.woff') format('woff');
+    font-weight: bold;
+    font-style: normal;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'Helvetica';
+    src: url('Helvetica-Compressed.woff2') format('woff2'),
+        url('Helvetica-Compressed.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'Helvetica';
+    src: url('Helvetica-Light.woff2') format('woff2'),
+        url('Helvetica-Light.woff') format('woff');
+    font-weight: 300;
+    font-style: normal;
+    font-display: swap;
+}
+
+.box-shadow {
+  box-shadow: 0px 3px 13px 4px rgba(0,0,0,0.1);
+}
+
+.button {
+  width: 190px;
+  height: 60px;
+  background-color: #0605AB;
+  border-radius: 15px;
+  text-align: center;
+  font-family: Helvetica;
+  font-size: 20px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
     </style>
   </head>
   <body class="bg-neutral-50 text-gray-900">
-    <header class="bg-white shadow-md sticky top-0 z-50">
-      <div
-        class="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center"
-      >
-      <a href="/elegance-paris/public/index.php">
-        <h1 class="text-2xl text-gray-800">🕴️ Élégance Parisienne</h1>
-      </a>
-      <form action="/elegance-paris/php/searchbar.php" method="get">
-        <input type="text" name="search" placeholder="Rechercher" class="border border-gray-300 rounded-md px-2 py-1">
-      </form>
-        <nav class="space-x-6 text-sm">
-          <?php if(isset($_SESSION['admin'])):?>
+    <header class="bg-[#F1F1F1] w-full">
+    <div class="flex justify-between items-center">
+          <a href="/elegance-paris/public/index.php" class="ml-[48px]" ><img src="images/logo.svg" alt="Logo Elegance Parisienne"></a>
+          <h1 class="text-[40px] font-[Cormorant] font-semibold mb-[23px] mr-[55px]">Élégance Parisienne</h1>
+          <a href="/elegance-paris/public/panier.php" class="mr-[48px] mb-[23px]"><img src="images/panier.svg" alt="Panier"></a>
+      </div>
+        <div class="flex justify-center items-center gap-x-[140px] mt-[-10px] pb-[10px] font-[Helvetica] italic">
+        <?php if(isset($_SESSION['admin'])):?>
           <a href="">page admin</a>
           <?php endif ?>
           <a href="/elegance-paris/public/index.php" class="text-gray-600 hover:text-black"
             >Accueil</a
-          >
-          <a href="/elegance-paris/public/panier.php" class="text-gray-600 hover:text-black"  // je met ici une autre adresse pour faire un test //
-            >Panier</a
           >
           <a href="/elegance-paris/public/collection.php" class="text-gray-600 hover:text-black">Nos Collections</a>
           <?php if(!isset($_SESSION['login'])): ?>
@@ -51,6 +127,10 @@
           <?php else: ?>
             <a href="/elegance-paris/php/logout.php" class="text-gray-600 hover:text-black">Deconnexion</a>
           <?php endif; ?>
+          <form action="/elegance-paris/php/searchbar.php" method="get">
+            <input type="text" name="search" placeholder="Rechercher" class="border border-gray-300 rounded-md px-2 py-1">
+          </form>
+        </div>
         </nav>
       </div>
     </header>
